3f81bab83b8465886db526783b1a94ef2a8dc561
Sudheer D <needforcode@users.noreply.github.com>
2018-06-23 12:16:18 -0700

    Combine Two Tables. #sql

---
+++ sql/CombineTwoTables.sql
@@ -0,0 +1,32 @@
+/*
+
+LeetCode:
+
+Combine Two Tables
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| PersonId    | int     |
+| FirstName   | varchar |
+| LastName    | varchar |
++-------------+---------+
+PersonId is the primary key column for this table.
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| AddressId   | int     |
+| PersonId    | int     |
+| City        | varchar |
+| State       | varchar |
++-------------+---------+
+AddressId is the primary key column for this table.
+
+Write a SQL query for a report that provides the following information for each person in the Person table, regardless if there is an address for each of those people.
+
+FirstName, LastName, City, State
+*/
+
+Select p.FirstName, p.LastName, a.City, a.State 
+from Person p LEFT OUTER JOIN Address a ON (p.PersonId = a.PersonId);
